215df336bc0deb5df2e15a6d225cdcbc789e5716
neurosync56 <syncneuro56@gmail.com>
2025-05-05 20:51:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-// script.js
-document.querySelectorAll('button').forEach(btn => {
-  btn.addEventListener('click', () => {
-    alert("Sorry fam 😔\nPayment gateway is still being coded...\nLike, the developers are asleep.\n\nBut here's a free meme:\nhttps://i.imgur.com/dhilshadintepari.gif");
-  });
-});
+
